0438cad009846dad7c24b543e8a171b3b8a0a760
abdelhaknezzari <abdelhak.nezzari@gmail.com>
2019-01-23 19:07:42 +0000

    Step 23: Custom Formatters 2

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,18 +1,19 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
-		function (Controller, JSONModel) {
-			"use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "../model/formatter"],
+	function (Controller, JSONModel, formatter) {
+		"use strict";
 
-			return Controller.extend("na.myProject2.controller.InvoiceList", {
+		return Controller.extend("na.myProject2.controller.InvoiceList", {
+			formatter: formatter,
 
-					onInit: function () {
-						var oData = {
-							currency:"EUR"
-						};
-						var oModel = new JSONModel(oData);
-						this.getView().setModel(oModel, "view");
-
-						}
-					});
+			onInit: function () {
+				var oData = {
+					currency: "EUR"
+				};
+				var oModel = new JSONModel(oData);
+				this.getView().setModel(oModel, "view");
 
 			}
-		);
+		});
+
+	}
+);
