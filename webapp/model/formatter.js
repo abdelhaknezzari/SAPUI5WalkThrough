02b48b3bcc2f268a527e6036053bfe3303d28613
abdelhaknezzari <abdelhak.nezzari@gmail.com>
2019-01-23 19:07:21 +0000

    Step 23: Custom Formatters

---
+++ webapp/model/formatter.js
@@ -0,0 +1,24 @@
+sap.ui.define([], function() {
+	
+	"use strict";
+	
+	return{ statusText : function(sStatus)
+	{
+		var oBundle = this.getView().getModel("i18n").getResourceBundle();
+		switch(sStatus)
+		{
+			case "A":
+				return oBundle.getText("invoicestatusA");
+			case "B":
+				return oBundle.getText("invoicestatusB");
+			case "C":
+				return oBundle.getText("invoicestatusC");
+			default:
+			    return oBundle.getText("invoicestatusA"); 
+		}
+		
+
+	}
+	};
+	
+});
